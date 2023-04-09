23905ac2b53b276f1b58ce884164719725830328
gouguoyin <245629560@qq.com>
2023-04-09 17:37:09 +0800

    v1.0.0

---
+++ admin/assets/website.8507a52f.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index.deaa6db5.js";function r(){return e.get({url:"/setting/base/detail"})}function i(t){return e.post({url:"/setting/base/save",params:t})}export{r as g,i as s};
